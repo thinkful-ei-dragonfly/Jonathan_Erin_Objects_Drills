4a22bcaf59f5365278baf685e4e7753902149b42
Erin Dustin <erincdustin@gmail.com>
2019-05-01 11:09:27 -0500

    added deleter drill

--- object.js
+++ object.js
@@ -19,42 +19,11 @@ function personMaker() {
     firstName: 'Paul',
     lastName: 'Jones',
     fullName: function() {
-      this.fullName = `${this.firstName} ${this.lastName}`;
-      return this.fullName;
+      return `${this.firstName} ${this.lastName}`;
     },
   };
   return person;
 }
-
-(function testPersonMaker() {
-  var person = personMaker();
-  if (typeof person !== 'object') {
-    console.error('ERROR: `personMaker` must return an object');
-    return false;
-  }
-  if (typeof person.fullName !== 'function') {
-    console.error('ERROR: `fullName` must be a method');
-    return false;
-  }
-  if (person.fullName() !== 'Paul Jones') {
-    console.error(
-      'ERROR: The value for `fullName` should be "Paul Jones" but was ' +
-        person.fullName()
-    );
-    return false;
-  }
-  person.firstName = 'Lisa';
-  person.lastName = 'Simpson';
-  if (person.fullName() !== 'Lisa Simpson') {
-    console.error(
-      '`personMaker` is not using self reference correctly. ' +
-        'When firstName set to "Lisa" and lastName set to "Simpson", ' +
-        'should return "Lisa Simpson" but returned ' +
-        person.fullName()
-    );
-  }
-  console.log('SUCCESS: `updateObject` works correctly!');
-})();
 
 
 const sampleObj = {
@@ -67,4 +36,13 @@ const sampleObj = {
 function keyDeleter(obj) {
   delete obj.foo;
   delete obj.bar;
+  return obj;
 }
+
+(function testKeyDeleter() {
+  var obj = keyDeleter({
+    foo: 'foo',
+    bar: 'bar',
+    bizz: 'bizz',
+    bang: 'bang',
+  });
