145fb27db825dafd775d1a755df435649ac819b3
Jonathan Moore <jmjonemoore@gmail.com>
2019-05-01 11:52:29 -0500

    Enroll in Summer School

--- object.js
+++ object.js
@@ -25,24 +25,26 @@ function personMaker() {
   return person;
 }
 
-
-const sampleObj = {
-  foo: 'foo',
-  bar: 'bar',
-  bizz: 'bizz',
-  bang: 'bang',
-};
-
 function keyDeleter(obj) {
   delete obj.foo;
   delete obj.bar;
   return obj;
 }
 
-(function testKeyDeleter() {
-  var obj = keyDeleter({
-    foo: 'foo',
-    bar: 'bar',
-    bizz: 'bizz',
-    bang: 'bang',
+function makeStudentsReport(data){
+  let result = [];
+  for(let i = 0; i < data.length; i++){
+    result.push(`${data[i].name}: ${data[i].grade}`);
+  }
+  return result;
+}
+
+function enrollInSummerSchool(students){
+  return students.map(student => {
+    return {    
+      name: student.name,
+      status: 'In Summer School',
+      course: student.course,
+    };
   });
+}
