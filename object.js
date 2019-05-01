896b5b09f97f9e1b3aca0dd78fbfe4d162b15bc0
Jonathan Moore <jmjonemoore@gmail.com>
2019-05-01 10:43:54 -0500

    Object Updater

--- object.js
+++ object.js
@@ -4,3 +4,11 @@ function createMyObject(){
     return 'hello';
   }};
 }
+
+function updateObject(obj){
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj['bizz'] = 'bizz';
+  obj['bang'] = 'bang';
+  return obj;
+}
