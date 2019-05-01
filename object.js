e0db5b55c783d91d27e04747f61cfa7fe1229ccd
Erin Dustin <erincdustin@gmail.com>
2019-05-01 12:53:40 -0500

    minor changes

--- object.js
+++ object.js
@@ -1,43 +1,43 @@
 'use strict';
-function createMyObject(){
-  return {foo: 'bar', answerToUniverse: 42, 'olly olly': 'oxen free', sayHello: function(){
-    return 'hello';
-  }};
-}
+// function createMyObject(){
+//   return {foo: 'bar', answerToUniverse: 42, 'olly olly': 'oxen free', sayHello: function(){
+//     return 'hello';
+//   }};
+// }
 
-function updateObject(obj){
-  obj.foo = 'foo';
-  obj.bar = 'bar';
-  obj['bizz'] = 'bizz';
-  obj['bang'] = 'bang';
-  return obj;
-}
+// function updateObject(obj){
+//   obj.foo = 'foo';
+//   obj.bar = 'bar';
+//   obj['bizz'] = 'bizz';
+//   obj['bang'] = 'bang';
+//   return obj;
+// }
 
 
-function personMaker() {
-  let person = {
-    firstName: 'Paul',
-    lastName: 'Jones',
-    fullName: function() {
-      return `${this.firstName} ${this.lastName}`;
-    },
-  };
-  return person;
-}
+// function personMaker() {
+//   let person = {
+//     firstName: 'Paul',
+//     lastName: 'Jones',
+//     fullName: function() {
+//       return `${this.firstName} ${this.lastName}`;
+//     },
+//   };
+//   return person;
+// }
 
-function keyDeleter(obj) {
-  delete obj.foo;
-  delete obj.bar;
-  return obj;
-}
+// function keyDeleter(obj) {
+//   delete obj.foo;
+//   delete obj.bar;
+//   return obj;
+// }
 
-function makeStudentsReport(data){
-  let result = [];
-  for(let i = 0; i < data.length; i++){
-    result.push(`${data[i].name}: ${data[i].grade}`);
-  }
-  return result;
-}
+// function makeStudentsReport(data){
+//   let result = [];
+//   for(let i = 0; i < data.length; i++){
+//     result.push(`${data[i].name}: ${data[i].grade}`);
+//   }
+//   return result;
+// }
 
 function enrollInSummerSchool(students){
   return students.map(student => {
@@ -48,3 +48,5 @@ function enrollInSummerSchool(students){
     };
   });
 }
+
+
