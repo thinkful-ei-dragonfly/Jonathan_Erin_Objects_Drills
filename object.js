0271dbd838385326918725ff115b94088d8aaf4d
Jonathan Moore <jmjonemoore@gmail.com>
2019-05-01 22:08:16 -0500

    Removing commented code

--- object.js
+++ object.js
@@ -1,161 +1,161 @@
 'use strict';
-// function createMyObject(){
-//   return {foo: 'bar', answerToUniverse: 42, 'olly olly': 'oxen free', sayHello: function(){
-//     return 'hello';
-//   }};
-// }
+function createMyObject(){
+  return {foo: 'bar', answerToUniverse: 42, 'olly olly': 'oxen free', sayHello: function(){
+    return 'hello';
+  }};
+}
 
-// function updateObject(obj){
-//   obj.foo = 'foo';
-//   obj.bar = 'bar';
-//   obj['bizz'] = 'bizz';
-//   obj['bang'] = 'bang';
-//   return obj;
-// }
-
-
-// function personMaker() {
-//   let person = {
-//     firstName: 'Paul',
-//     lastName: 'Jones',
-//     fullName: function() {
-//       return `${this.firstName} ${this.lastName}`;
-//     },
-//   };
-//   return person;
-// }
-
-// function keyDeleter(obj) {
-//   delete obj.foo;
-//   delete obj.bar;
-//   return obj;
-// }
-
-// function makeStudentsReport(data){
-//   let result = [];
-//   for(let i = 0; i < data.length; i++){
-//     result.push(`${data[i].name}: ${data[i].grade}`);
-//   }
-//   return result;
-// }
-
-//why the second return?
-// function enrollInSummerSchool(students){
-//   return students.map(student => {
-//     return {    
-//       name: student.name,
-//       status: 'In Summer School',
-//       course: student.course,
-//     };
-//   });
-// }
-
-// const scratchData = [
-//   { id: 22, foo: 'bar' },
-//   { id: 28, foo: 'bizz' },
-//   { id: 19, foo: 'bazz' },
-// ];
-
-// function findById(items, idNum) {
-//   return items.find(value => value.id === idNum);
-// }
-
-// let example = findById(scratchData, 19);
-// console.log(example);
+function updateObject(obj){
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj['bizz'] = 'bizz';
+  obj['bang'] = 'bang';
+  return obj;
+}
 
 
-// function validateKeys(object, expectedKeys) {
-//   if (Object.keys(object).length !== expectedKeys.length) {
-//     return false;
-//   }
+function personMaker() {
+  let person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    fullName: function() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+  };
+  return person;
+}
 
-//   for (let i = 0; i <expectedKeys.length; i++) {
-//     if (!Object.keys(object).find(expKey => expKey === expectedKeys[i])) {
-//       return false;
-//     }
-//   } 
-//   //why doesn't this work?
-//   // else {for (let i =0; i < expectedKeys.length; i++) {
-//   //   if (expectedKeys[i] in object) {
-//   //     return true;}
-// }
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
 
-// const objectA = {
-//   id: 2,
-//   name: 'Jane Doe',
-//   age: 34,
-//   city: 'Chicago',
-// };
+function makeStudentsReport(data){
+  let result = [];
+  for(let i = 0; i < data.length; i++){
+    result.push(`${data[i].name}: ${data[i].grade}`);
+  }
+  return result;
+}
 
-// const objectB = {
-//   id: 3,
-//   age: 33,
-//   city: 'Peoria',
-//   hello: 'hi',
-// };
+// why the second return?
+function enrollInSummerSchool(students){
+  return students.map(student => {
+    return {    
+      name: student.name,
+      status: 'In Summer School',
+      course: student.course,
+    };
+  });
+}
 
-// const expectedKeys = ['id', 'name', 'age', 'city'];
+const scratchData = [
+  { id: 22, foo: 'bar' },
+  { id: 28, foo: 'bizz' },
+  { id: 19, foo: 'bazz' },
+];
 
-// let test1 = validateKeys(objectB, expectedKeys);
-// console.log(test1);
+function findById(items, idNum) {
+  return items.find(value => value.id === idNum);
+}
 
-// const loaf = {flour: 300, water: 210};
-// for(let key in loaf){
-//   console.log(key);
-// }
-// loaf.hydration = function(){
-//   return ((this.water/this.flour) * 100);
-// };
-// console.log(loaf.hydration());
-
-// const obj = {foo: 'foo', bar: 'bar', fum: 25, quux: 70};
-// for(let key in obj){
-//   console.log(`${key}: ${obj[key]}`);
-// }
-
-
-// const obj2 = {meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']}
-// console.log(obj2.meals[3]);
+let example = findById(scratchData, 19);
+console.log(example);
 
 
-// const obj3 = {name: 'Bud', 'job title': 'Programmer'};
-// const obj4 = {name: 'Sally', 'job title': 'Student'};
-// const obj5 = {name: 'Erin', 'job title': 'Unemployed'};
+function validateKeys(object, expectedKeys) {
+  if (Object.keys(object).length !== expectedKeys.length) {
+    return false;
+  }
 
-// const objArr = [obj3, obj4, obj5];
-// objArr.forEach(object => console.log(`Job: ${object['job title']}, Name: ${object['name']}`));
+  for (let i = 0; i <expectedKeys.length; i++) {
+    if (!Object.keys(object).find(expKey => expKey === expectedKeys[i])) {
+      return false;
+    }
+  } 
+  //why doesn't this work?
+  // else {for (let i =0; i < expectedKeys.length; i++) {
+  //   if (expectedKeys[i] in object) {
+  //     return true;}
+}
+
+const objectA = {
+  id: 2,
+  name: 'Jane Doe',
+  age: 34,
+  city: 'Chicago',
+};
+
+const objectB = {
+  id: 3,
+  age: 33,
+  city: 'Peoria',
+  hello: 'hi',
+};
+
+const expectedKeys = ['id', 'name', 'age', 'city'];
+
+let test1 = validateKeys(objectB, expectedKeys);
+console.log(test1);
+
+const loaf = {flour: 300, water: 210};
+for(let key in loaf){
+  console.log(key);
+}
+loaf.hydration = function(){
+  return ((this.water/this.flour) * 100);
+};
+console.log(loaf.hydration());
+
+const obj = {foo: 'foo', bar: 'bar', fum: 25, quux: 70};
+for(let key in obj){
+  console.log(`${key}: ${obj[key]}`);
+}
 
 
-//properties that aren't there
-
-// function Employee(name, jobTitle) {
-//   this.name = name;
-//   this.jobTitle = jobTitle;
-// }
-
-// function Owner(name, jobTitle) {
-//   this.name = name;
-//   this.jobTitle = jobTitle;
-// }
+const obj2 = {meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']}
+console.log(obj2.meals[3]);
 
 
-// const obj6 = new Employee('Bud', 'Programmer');
-// const obj7 = new Employee('Sally', 'Student');
-// const obj8 = new Owner('Erin', 'Unemployed');
+const obj3 = {name: 'Bud', 'job title': 'Programmer'};
+const obj4 = {name: 'Sally', 'job title': 'Student'};
+const obj5 = {name: 'Erin', 'job title': 'Unemployed'};
 
-// Employee.prototype.boss = 'Erin';
+const objArr = [obj3, obj4, obj5];
+objArr.forEach(object => console.log(`Job: ${object['job title']}, Name: ${object['name']}`));
 
-// console.log(obj6);
 
-// const objArr2 = [obj6, obj7, obj8];
+properties that aren't there
 
-// objArr2.forEach(function(object) {
-//   if ('boss' in object) {
-//     console.log(`${object.jobTitle} ${object['name']} reports to ${object['boss']}`);
-//   } else {
-//     console.log(`${object['jobTitle']} ${object['name']} doesn't report to anybody.`);
-//   }
-// });
+function Employee(name, jobTitle) {
+  this.name = name;
+  this.jobTitle = jobTitle;
+}
+
+function Owner(name, jobTitle) {
+  this.name = name;
+  this.jobTitle = jobTitle;
+}
+
+
+const obj6 = new Employee('Bud', 'Programmer');
+const obj7 = new Employee('Sally', 'Student');
+const obj8 = new Owner('Erin', 'Unemployed');
+
+Employee.prototype.boss = 'Erin';
+
+console.log(obj6);
+
+const objArr2 = [obj6, obj7, obj8];
+
+objArr2.forEach(function(object) {
+  if ('boss' in object) {
+    console.log(`${object.jobTitle} ${object['name']} reports to ${object['boss']}`);
+  } else {
+    console.log(`${object['jobTitle']} ${object['name']} doesn't report to anybody.`);
+  }
+});
 
 
 //cracking the code
